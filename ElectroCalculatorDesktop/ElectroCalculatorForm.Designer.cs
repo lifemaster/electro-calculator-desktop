5b89546735ec72ac46c9a9e37f0385787a1138b2
lifemaster <kostya.shyika@gmail.com>
2020-11-11 11:21:44 +0200

    Add project files.

---
+++ ElectroCalculatorDesktop/ElectroCalculatorForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ElectroCalculatorDesktop
+{
+    partial class ElectroCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ElectroCalculatorForm));
+            this.prevValueLabel = new System.Windows.Forms.Label();
+            this.currentValueLabel = new System.Windows.Forms.Label();
+            this.prevValueTextBox = new System.Windows.Forms.TextBox();
+            this.currentValueTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // prevValueLabel
+            // 
+            this.prevValueLabel.AutoSize = true;
+            this.prevValueLabel.Location = new System.Drawing.Point(12, 27);
+            this.prevValueLabel.Name = "prevValueLabel";
+            this.prevValueLabel.Size = new System.Drawing.Size(181, 13);
+            this.prevValueLabel.TabIndex = 0;
+            this.prevValueLabel.Text = "Предыдущее показание счетчика:";
+            // 
+            // currentValueLabel
+            // 
+            this.currentValueLabel.AutoSize = true;
+            this.currentValueLabel.Location = new System.Drawing.Point(12, 69);
+            this.currentValueLabel.Name = "currentValueLabel";
+            this.currentValueLabel.Size = new System.Drawing.Size(166, 13);
+            this.currentValueLabel.TabIndex = 1;
+            this.currentValueLabel.Text = "Текущее показаниие счетчика:";
+            // 
+            // prevValueTextBox
+            // 
+            this.prevValueTextBox.Location = new System.Drawing.Point(199, 24);
+            this.prevValueTextBox.Name = "prevValueTextBox";
+            this.prevValueTextBox.Size = new System.Drawing.Size(120, 20);
+            this.prevValueTextBox.TabIndex = 2;
+            this.prevValueTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PrevValueTextBox_KeyDown);
+            // 
+            // currentValueTextBox
+            // 
+            this.currentValueTextBox.Location = new System.Drawing.Point(199, 66);
+            this.currentValueTextBox.Name = "currentValueTextBox";
+            this.currentValueTextBox.Size = new System.Drawing.Size(120, 20);
+            this.currentValueTextBox.TabIndex = 3;
+            this.currentValueTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CurrentValueTextBox_KeyDown);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(135, 116);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Рассчитать";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ElectroCalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 151);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.currentValueTextBox);
+            this.Controls.Add(this.prevValueTextBox);
+            this.Controls.Add(this.currentValueLabel);
+            this.Controls.Add(this.prevValueLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ElectroCalculatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор электроэнергии";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label prevValueLabel;
+        private System.Windows.Forms.Label currentValueLabel;
+        private System.Windows.Forms.TextBox prevValueTextBox;
+        private System.Windows.Forms.TextBox currentValueTextBox;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ ElectroCalculatorDesktop/ElectroCalculatorForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectroCalculatorDesktop
+{
+    public partial class ElectroCalculatorForm : Form
+    {
+        static readonly decimal lessThan100kwtTariff = 0.90M;
+        static readonly decimal moreThan100kwtTariff = 1.68M;
+
+        public ElectroCalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PrevValueTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                calculateButton.PerformClick();
+        }
+
+        private void CurrentValueTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                calculateButton.PerformClick();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            string prevValue = prevValueTextBox.Text;
+            string currentValue = currentValueTextBox.Text;
+
+            int[] values = this.ValidateUserInput(prevValue, currentValue);
+
+            if (values == null)
+            {
+                return;
+            }
+
+            int prevValueInt = values[0];
+            int currentValueInt = values[1];
+
+            string resultMessage = this.GetCalculationResult(prevValueInt, currentValueInt);
+
+            this.ShowCalculateResultMessageBox(resultMessage);
+        }
+
+        private int[] ValidateUserInput(string prevValue, string currentValue)
+        {
+            if (!Int32.TryParse(prevValue, out int prevValueInt))
+            {
+                this.ShowErrorMessageBox("Предыдущее показание счетчика должно быть целым числом и меньше, чем 2147483647");
+                return null;
+            }
+
+            if (!Int32.TryParse(currentValue, out int currentValueInt))
+            {
+                this.ShowErrorMessageBox("Текущее показание счетчика должно быть целым числом и меньше, чем 2147483647");
+                return null;
+            }
+
+            if (prevValueInt <= 0)
+            {
+                this.ShowErrorMessageBox("Предыдущее показание счетчика должно быть больше нуля");
+                return null;
+            }
+
+            if (currentValueInt <= 0)
+            {
+                this.ShowErrorMessageBox("Текущее показание счетчика должно быть больше нуля");
+                return null;
+            }
+
+            if (prevValueInt > currentValueInt)
+            {
+                this.ShowErrorMessageBox("Предыдущее показание не может быть больше, чем текущее");
+                return null;
+            }
+
+            if (prevValueInt == currentValueInt)
+            {
+                this.ShowCalculateResultMessageBox("Вы не пользовались электричеством, поэтому ничего не должны платить");
+                return null;
+            }
+
+            return new int[] { prevValueInt, currentValueInt };
+        }
+
+        private void ShowErrorMessageBox(string textError)
+        {
+            MessageBox.Show(textError, "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ShowCalculateResultMessageBox(string textError)
+        {
+            MessageBox.Show(textError, "Результат расчета", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private string GetCalculationResult(int prevIntValue, int currentIntValue)
+        {
+            int totalCount = currentIntValue - prevIntValue;
+            string result = $"Вы израсходовали {totalCount} кВт·ч:\n\n";
+
+            if (totalCount <= 100)
+            {
+                result += $"{totalCount} кВт·ч · {lessThan100kwtTariff} грн/кВт·ч = {totalCount * lessThan100kwtTariff} грн";
+                result += "\n\n";
+                result += $"Итого к оплате: {totalCount * lessThan100kwtTariff} грн";
+            }
+            else
+            {
+                result += $"100 кВт·ч · {lessThan100kwtTariff} грн/кВт·ч = {lessThan100kwtTariff * 100} грн";
+                result += "\n";
+                result += $"{totalCount - 100} кВт·ч · {moreThan100kwtTariff} грн/кВт·ч = {(totalCount - 100) * moreThan100kwtTariff} грн";
+                result += "\n\n";
+                result += $"Итого к оплате: {lessThan100kwtTariff * 100 + (totalCount - 100) * moreThan100kwtTariff} грн";
+            }
+
+            return result;
+        }
+    }
+}
